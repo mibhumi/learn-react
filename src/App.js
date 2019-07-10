c553440cd9962e45daf2a536a75caa9432d7b4a6
“mibhumi” <bhumipatel1196@gmail.com>
2019-07-10 12:36:29 +0530

    Use two way data binding and toggle method to display data

--- src/App.js
+++ src/App.js
@@ -3,59 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-// functional component
-// const App = props => {
-//
-//     const [personsState, setPersonsState] = useState({
-//         persons: [
-//             { name: 'Max', age: 28 },
-//             { name: 'Manu', age: 29 },
-//             { name: 'Stephanie', age: 26 }
-//         ]
-//     });
-//
-//     const [otherState, setOtherState] = useState({
-//         otherState: 'some other value'
-//     });
-//
-//     const switchNameHandler = () => {
-//         setPersonsState({
-//             persons: [
-//                 { name: 'Maximilian', age: 29 },
-//                 { name: 'Manu', age: 29 },
-//                 { name: 'Stephanie', age: 29 }
-//             ]
-//         });
-//     };
-//
-//
-//     return (
-//         <div className="App">
-//             <h1>Hi, I'm a React App</h1>
-//             <p>This is really working!</p>
-//             <button onClick={ switchNameHandler }>Change state</button>
-//             <Person
-//                 name={personsState.persons[0].name}
-//                 age={personsState.persons[0].age}
-//             />
-//             <Person
-//                 name={personsState.persons[1].name}
-//                 age={personsState.persons[1].age}
-//             >
-//                 My Hobbies: Racing
-//             </Person>
-//             <Person
-//                 name={personsState.persons[2].name}
-//                 age={personsState.persons[2].age}
-//             />
-//         </div>
-//     );
-// }
-
-
-
 // class based example
-
 class App extends Component {
     state = {
         persons: [
@@ -134,20 +82,5 @@ class App extends Component {
         );
     }
 }
-
-
-
-// default
-
-// function App() {
-//   return (
-//       <div className="App">
-//         <h1>Hello World!</h1>
-//         <h2>This is working</h2>
-//         <Person name="Bhumi" age="22" />
-//         <person name="Om" age="16">Children</person>
-//       </div>
-//   );
-// }
 
 export default App;
