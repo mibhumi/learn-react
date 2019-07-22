030ba9329a466ae33bf0ee25e722c29e010c1e39
“mibhumi” <bhumipatel1196@gmail.com>
2019-07-22 11:54:05 +0530

    Counter demo using redux

---
+++ src/store/reducer.js
@@ -0,0 +1,32 @@
+const initState = {
+    counter: 0
+}
+
+const reducer = (state = initState, action) => {
+
+    	switch (action.type) {
+	    case 'INCREMENT':
+		return {
+		    counter: state.counter + 1
+		}
+
+	    case 'DECREMENT':
+		return {
+		    counter: state.counter - 1
+		}
+
+	    case 'ADD':
+		return {
+		    counter: state.counter + action.value
+		}
+
+	    case 'SUB':
+		return {
+		    counter: state.counter - action.value
+		}
+	}
+
+	return state;
+}
+
+export default reducer;
