26a3be45533b8d509288c53929891cd970c538f1
“mibhumi” <bhumipatel1196@gmail.com>
2019-07-23 12:24:50 +0530

    Fix counter issue

--- src/store/store.js
+++ src/store/store.js
@@ -2,41 +2,45 @@ import { createStore, combineReducers } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 import reducerCounter from './reducerCounter';
 
-const initState = {
-    counter: 0,
+const initForm = {
+    name: ''
 }
 
-const reducer = (state = initState, action) => {
+// const initState = {
+//     counter: 0,
+// }
+//
+// const reducerCount = (state = initState, action) => {
+//
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return {
+//                 counter: state.counter + 1
+//             }
+//
+//         case 'DECREMENT':
+//             return {
+//                 counter: state.counter - 1
+//             }
+//
+//         case 'ADD':
+//             return {
+//                 counter: state.counter + action.value
+//             }
+//
+//         case 'SUB':
+//             return {
+//                 counter: state.counter - action.value
+//             }
+//     }
+//
+//     return state;
+// }
 
-    switch (action.type) {
-        case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
-
-        case 'DECREMENT':
-            return {
-                counter: state.counter - 1
-            }
-
-        case 'ADD':
-            return {
-                counter: state.counter + action.value
-            }
-
-        case 'SUB':
-            return {
-                counter: state.counter - action.value
-            }
-    }
-
-    return state;
-}
-
-// const reducer = combineReducers({
-//     form: reduxFormReducer, // mounted under "form"
-//     reducerCount: reducerCount,
-// });
+const reducer = combineReducers({
+    form: reduxFormReducer, // mounted under "form"
+    initForm
+});
 
 // const store = (window.devToolsExtension
 //     ? window.devToolsExtension()(createStore)
