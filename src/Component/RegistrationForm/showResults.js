7266a05cdc66e3cf0db54512d8eb8d1526394fe3
“mibhumi” <bhumipatel1196@gmail.com>
2019-07-24 18:44:32 +0530

    Convert showResult into a arrow function

--- src/Component/RegistrationForm/showResults.js
+++ src/Component/RegistrationForm/showResults.js
@@ -1,4 +1,6 @@
-export default (async function showResults(values) {
+const showResults = (values) => {
     // window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
     console.log(JSON.stringify(values,null,2));
-});
+}
+
+export default showResults;
